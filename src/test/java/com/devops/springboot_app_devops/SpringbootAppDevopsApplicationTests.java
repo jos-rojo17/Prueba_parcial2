bcc3b32de461676e165792335681fe646ff9c071
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-05-20 17:05:50 -0400

    Primer commit

---
+++ src/test/java/com/devops/springboot_app_devops/SpringbootAppDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.springboot_app_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
